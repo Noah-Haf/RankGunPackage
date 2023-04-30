03af2de52257a19d21fa0ab26b7b2e3bf5a0a8cb
Noah <61884850+Noah-Haf@users.noreply.github.com>
2023-04-30 21:52:56 +0100

    Update testing.py

--- tests/testing.py
+++ tests/testing.py
@@ -3,10 +3,13 @@ import sys
 
 Api_Token = sys.argv[0]
 
+
 Workspace = rankgun.RankGunAPI(Api_Token, 1)
 
 Attempt1 = Workspace.promote(username="RankGunTests")
 Attempt2 = Workspace.demote(username="RankGunTests")
+
+print(Attempt1)
 
 if Attempt1["status_code"] != 200 or Attempt2["status_code"] != 200: 
     raise("Something went wrong")
