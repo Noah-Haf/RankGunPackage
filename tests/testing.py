626f95f62895273f3fbcd6681772b837ac23cf52
Noah <61884850+Noah-Haf@users.noreply.github.com>
2023-04-30 22:41:52 +0100

    Update testing.py

--- tests/testing.py
+++ tests/testing.py
@@ -1,17 +1,20 @@
-import rankgun 
+import rankgun
 import sys
 
 Api_Token = sys.argv[0]
 
-print("let's go")
-Workspace = rankgun.RankGunAPI(Api_Token, 1)
+async def main():
+    print("let's go")
+    Workspace = rankgun.RankGunAPI(Api_Token, 1)
 
-Attempt1 = Workspace.promote(username="RankGunTests")
-Attempt2 = Workspace.demote(username="RankGunTests")
+    Attempt1 = await Workspace.promote(username="RankGunTests")
+    Attempt2 = await Workspace.demote(username="RankGunTests")
 
-print(Attempt1)
+    print(Attempt1)
 
-if Attempt1["status_code"] != 200 or Attempt2["status_code"] != 200: 
-    raise("Something went wrong")
-else:
-    print("All went well")
+    if Attempt1["status_code"] != 200 or Attempt2["status_code"] != 200: 
+        raise Exception("Something went wrong")
+    else:
+        print("All went well")
+
+await main()
