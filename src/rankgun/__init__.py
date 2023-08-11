1072283c359ac7c9e7327f390f3cf6905617db19
Noah <61884850+Noah-Haf@users.noreply.github.com>
2023-08-11 19:54:35 +0100

    Fix method not allowed

--- src/rankgun/__init__.py
+++ src/rankgun/__init__.py
@@ -1,6 +1,5 @@
-import aiohttp
-
-BASE_URL = "http://api.rankgun.works"
+import requests
+BASE_URL = "https://api.rankgun.works"
 
 
 class RankGun:
@@ -9,28 +8,22 @@ class RankGun:
     def __init__(self, api_token, workspace_id):
         self.api_key = api_token
         self.workspace_id = workspace_id
-        self.session = aiohttp.ClientSession(headers={"api-token": self.api_key})
 
-    async def _get(self, endpoint, params=None):
-        """Make a GET request to the RankGun API."""
+
+    def _request(self, method, endpoint, params=None, data=None):
         url = f"{BASE_URL}{endpoint}"
-        async with self.session.get(url, params=params) as response:
-            response.raise_for_status()
-            return await response.json()
+        response = requests.request(method, url, params=params, data=data, headers={"api-token": self.api_key})
+        response.raise_for_status()
+        return response.json()
 
-    async def _post(self, endpoint, params=None):
-        """Make a GET request to the RankGun API."""
-        url = f"{BASE_URL}{endpoint}"
-        async with self.session.post(url, params=params) as response:
-            response.raise_for_status()
-            return await response.json()
 
-    async def get_status(self):
+
+    def get_status(self):
         """Get the status of the RankGun API."""
         endpoint = "/status"
-        return await self._get(endpoint)
+        return self._request("GET", endpoint)
 
-    async def promote(self, username=None, User_Id=None):
+    def promote(self, username=None, User_Id=None):
         """Promote a user. +1 rank"""
         endpoint = "/ranking/promote"
         params = {"workspace_id": self.workspace_id}
@@ -39,9 +32,9 @@ class RankGun:
             params["username"] = username
         elif User_Id is not None:
             params["user_id"] = User_Id
-        return await self._post(endpoint, params=params)
+        return self._request("POST", endpoint, params=params)
 
-    async def demote(self, username=None, User_Id=None):
+    def demote(self, username=None, User_Id=None):
         """Demote a user. -1 Rank"""
         endpoint = "/ranking/demote"
         params = {"workspace_id": self.workspace_id}
@@ -50,9 +43,9 @@ class RankGun:
             params["username"] = username
         elif User_Id is not None:
             params["user_id"] = User_Id
-        return await self._post(endpoint, params=params)
+        return self._request("POST", endpoint, params=params)
 
-    async def setrank(self, rank, username=None, User_Id=None):
+    def setrank(self, rank, username=None, User_Id=None):
         """Set the rank of a user. * Rank"""
         endpoint = "/ranking/set-rank"
         params = {"workspace_id": self.workspace_id, "rank": rank}
@@ -61,9 +54,9 @@ class RankGun:
             params["username"] = username
         elif User_Id is not None:
             params["user_id"] = User_Id
-        return await self._post(endpoint, params=params)
+        return self._request("POST", endpoint, params=params)
 
-    async def exile(self, username=None, User_Id=None):
+    def exile(self, username=None, User_Id=None):
         """Removes players from group."""
         endpoint = "/ranking/exile"
         params = {"workspace_id": self.workspace_id}
@@ -72,10 +65,10 @@ class RankGun:
             params["username"] = username
         elif User_Id is not None:
             params["user_id"] = User_Id
-        return await self._post(endpoint, params=params)
+        return self._request("POST", endpoint, params=params)
 
-    async def shout(self, shout_text):
+    def shout(self, shout_text):
         """Shouts to group."""
         endpoint = "/utils/shout"
         params = {"workspace_id": self.workspace_id, "shout_text": shout_text}
-        return await self._post(endpoint, params=params)
+        return self._request("POST", endpoint, params=params)
